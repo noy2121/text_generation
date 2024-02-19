fd40c3b7eb6e05786971e0b18f07219f462c632e
Noy <noy.rahmani@viisights.com>
2024-02-19 11:02:37 +0200

    add positional encoder

---
+++ text_generator/model/positional_encoder.py
@@ -0,0 +1,20 @@
+import math
+import torch
+import torch.nn as nn
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, model_dim, dropout=0.1, max_len=5000):
+        super().__init__()
+
+        self.dropout = nn.Dropout(p=dropout)
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, model_dim, 2) * (-math.log(10000.0) / model_dim))
+        pe = torch.zeros(max_len, 1, model_dim)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
