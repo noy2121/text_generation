976e5548d8e08e81302e1167485ca456b7171387
Noy <noy.rahmani@viisights.com>
2024-02-20 14:34:40 +0200

    fix transformer model: - update masks - remove fc layer from encoder_output

--- text_generator/model/transformer.py
+++ text_generator/model/transformer.py
@@ -9,42 +9,37 @@ from .positional_encoder import PositionalEncoding
 
 
 class TransformerModel(nn.Module):
-    def __init__(self, ntokens, model_dim, nheads, ff_dim, nlayers, dropout=0.1):
+    def __init__(self, ntokens, model_dim, nheads, ff_dim, nlayers, seq_len, dropout=0.1):
         super().__init__()
 
         self.model_type = 'Transformer'
-        self.pos_encoder = PositionalEncoding(model_dim, dropout)
+        self.pos_encoder = PositionalEncoding(model_dim, seq_len, dropout)
 
         encoder_layers = TransformerEncoderLayer(model_dim, nheads, ff_dim, dropout)
         decoder_layers = TransformerDecoderLayer(model_dim, nheads, ff_dim, dropout)
         self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
         self.transformer_decoder = TransformerDecoder(decoder_layers, nlayers)
-        self.embedding = nn.Embedding(ntokens, model_dim)
+        self.enc_embedding = nn.Embedding(ntokens, model_dim)
+        self.dec_embedding = nn.Embedding(ntokens, model_dim)
         self.model_dim = model_dim
         self.fc = nn.Linear(model_dim, ntokens)
-
-        self.init_weights()
-
-    def init_weights(self):
-        initrange = 0.1
-        self.embedding.weight.data.uniform_(-initrange, initrange)
-        self.fc.bias.data.zero_()
-        self.fc.weight.data.uniform_(-initrange, initrange)
 
     def forward(self, src, tgt):
         """
         :param src: Tensor, shape [seq_len, batch_size]
-        :param src_mask: Tensor, shape [seq_len, seq_len]
+        :param tgt: Tensor, shape [seq_len, batch_size]
         :return: output tensor of shape [seq_len, batch_size, ntokens]
         """
-        src = self.embedding(src) * math.sqrt(self.model_dim)
-        src = self.pos_encoder(src)
-        src_mask = nn.Transformer.generate_square_subsequent_mask(len(src))
-        tgt_mask = nn.Transformer.generate_square_subsequent_mask(len(tgt))
-        output = self.transformer_encoder(src, src_mask)
-        encoder_output = self.fc(output)
+        src_mask = nn.Transformer.generate_square_subsequent_mask(src.size(0))
+        tgt_mask = nn.Transformer.generate_square_subsequent_mask(tgt.size(0))
 
-        output = self.transformer_decoder(tgt, encoder_output, tgt_mask)
-        output = self.fc(output)
+        src = self.enc_embedding(src) * math.sqrt(self.model_dim)
+        src = self.pos_encoder(src)
+        tgt = self.dec_embedding(tgt) * math.sqrt(self.model_dim)
+        tgt = self.pos_encoder(tgt)
+
+        encoder_output = self.transformer_encoder(src, src_mask)
+        decoder_output = self.transformer_decoder(tgt, encoder_output, tgt_mask=tgt_mask, memory_mask=src_mask)
+        output = self.fc(decoder_output)
 
         return output
